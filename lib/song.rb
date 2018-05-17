545de431bb0b7f911f72d993c720c8ff9797d523
Anthony Lam <al3101@nyu.edu>
2018-05-17 00:43:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,50 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    self.all << song 
+    return song 
+  end 
+  
+  def self.new_by_name(name) 
+    song = self.create
+    song.name = name 
+    return song 
+  end 
+  
+  def self.create_by_name(name) 
+    return self.new_by_name(name) 
+  end
+  
+  def self.find_by_name(name) 
+    self.all.each do |song| 
+      if song.name == name 
+        return song 
+      end
+    end 
+  end 
+  
+  def self.find_or_create_by_name(name) 
+    if self.find_by_name(name)
+      return self.find_by_name(name)
+    else
+      return self.create_by_name(name)
+    end 
+  end
+      
+  def self.alphabetical 
+    return self.all.sort_by{|song|song[0]}
+  end 
+  
+  def self.new_from_filename(filename)
+  end 
+  
+  def self.create_from_filename(filename)
+  end 
 
+  def self.destroy_all 
+    self.all.clear 
+  end 
 end
